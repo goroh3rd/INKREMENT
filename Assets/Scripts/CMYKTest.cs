b036f6f4c5b45119e6ca72af72b8d000ed387771
goroh <65319834+goroh3rd@users.noreply.github.com>
2025-12-24 15:21:42 +0900

    減法混色が機能することを確認

--- Assets/Scripts/CMYKTest.cs
+++ Assets/Scripts/CMYKTest.cs
@@ -6,18 +6,16 @@ public class CMYKTest : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     void Start()
     {
-        Color rgbColor = cmykColor.ToRGB();
-        Debug.Log($"CMYK({cmykColor.c}, {cmykColor.m}, {cmykColor.y}, {cmykColor.k}) to RGB: {rgbColor}");
-
-        CMYK convertedBack = CMYK.FromRGB(rgbColor);
-        Debug.Log($"RGB({rgbColor.r}, {rgbColor.g}, {rgbColor.b}) back to CMYK: ({convertedBack.c}, {convertedBack.m}, {convertedBack.y}, {convertedBack.k})");
+        cmykColor = CMYK.Magenta + CMYK.Yellow;
+        UpdateColor(cmykColor);
     }
     private void OnValidate()
     {
-        cmykColor.c = Mathf.Clamp01(cmykColor.c);
-        cmykColor.m = Mathf.Clamp01(cmykColor.m);
-        cmykColor.y = Mathf.Clamp01(cmykColor.y);
-        cmykColor.k = Mathf.Clamp01(cmykColor.k);
+        UpdateColor(cmykColor);
+    }
+    private void UpdateColor(CMYK newColor)
+    {
+        cmykColor = newColor;
         if (spriteRenderer != null)
         {
             spriteRenderer.color = cmykColor.ToRGB();
